5419f6f23cf25858777ff1c9e2e9868ac67a8a9f
Marcelo Santos <rocha.mer21@gmail.com>
2021-09-28 11:50:21 -0300

    limitando as TextFields a aceitar somente numeros

--- src/main/java/com/example/demo/ViewController.java
+++ src/main/java/com/example/demo/ViewController.java
@@ -1,22 +1,52 @@
 package com.example.demo;
 
 import com.example.demo.gui.util.Alerts;
+import com.example.demo.gui.util.Constraints;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
-public class ViewController {
+import java.net.URL;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class ViewController implements Initializable {
 
     @FXML
-    private Button btTest;
+    private TextField txtNumber1;
 
     @FXML
-    private Label lbTest;
+    private TextField txtNumber2;
 
     @FXML
-    public void onBtTestAction(){
-        Alerts.showAlert("Alert Title", "Alert Header", "Hello", Alert.AlertType.INFORMATION);
+    private Label lblResult;
+
+    @FXML
+    private Button btSum;
+
+
+    @FXML
+    public void onBtSumAction() {
+        try {
+            Locale.setDefault(Locale.US);
+            double number1 = Double.parseDouble(txtNumber1.getText());
+            double number2 = Double.parseDouble(txtNumber2.getText());
+            double sum = number1 + number2;
+
+            lblResult.setText(String.format("%.2f", sum));
+        } catch (NumberFormatException e) {
+            Alerts.showAlert("Error", "Parse error", e.getMessage(), Alert.AlertType.ERROR);
+        }
+
+
     }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        Constraints.setTextFieldDouble(txtNumber1);
+        Constraints.setTextFieldDouble(txtNumber2);
+    }
 }
